7eb7aba4df4d2e147d6d5f95f02ee9e17ae1b032
Localhost07 <145258687+error-ph4ntom@users.noreply.github.com>
2023-10-16 19:24:35 +0530

    Update and rename phisher.txt to phisher.sh

--- phisher.sh
+++ phisher.sh
@@ -84,7 +84,7 @@ __version__="2.3.5"
 
 ## DEFAULT HOST & PORT
 HOST='127.0.0.1'
-PORT='0707'
+PORT='8080'
 
 ## ANSI colors (FG & BG)
 RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
